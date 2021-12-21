c508d98dfea8fc69a068b8dcd7b42972f989a8e4
DC\Shams <mshamsj@gmail.com>
2021-12-21 17:39:12 +0330

    Add documentation, Pre/Post process.

--- SBO-CMDLine/Command.cs
+++ SBO-CMDLine/Command.cs
@@ -6,19 +6,43 @@ using Mono.Options;
 
 namespace SBO_CMDLine
 {
-    public abstract partial class Command
+    /// <summary>
+    /// Base class to be used as template for commands
+    /// </summary>
+    public abstract class Command
     {
+        /// <summary>
+        /// Indicate the the command should print verbose information.
+        /// </summary>
         protected bool VerboseMode = false;
 
+        /// <summary>
+        /// Description about command purpose.
+        /// </summary>
         public abstract string Description { get; }
+
+        /// <summary>
+        /// Help and usage of the command.
+        /// </summary>
         public abstract string Help { get; }
+
+        /// <summary>
+        /// OptionSet and switches defined ny the command. Don't define single character commands.
+        /// </summary>
         public abstract OptionSet Options { get; }
 
+        /// <summary>
+        /// Print command usage and information.
+        /// </summary>
         public void ShowHelp()
         {
             this.Options?.WriteOptionDescriptions(Console.Out);
         }
 
+        /// <summary>
+        /// Process input arguments by the command.
+        /// </summary>
+        /// <param name="args"></param>
         public void Process(string[] args)
         {
             try
@@ -28,13 +52,25 @@ namespace SBO_CMDLine
                     ShowHelp();
                 }
                 else
+                {
+                    PreProcess();
                     this.Options?.Parse(args);
+                    PostProcess();
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Error! {e.Message}\n");
                 ShowHelp();
             }
+        }
+
+        public virtual void PostProcess()
+        {
+        }
+
+        public virtual void PreProcess()
+        {
         }
     }
 }
